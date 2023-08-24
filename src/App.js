44b465095fed920c979f95755cf13470d49ca838
Q SYED AJMAL <syedajmalq@gmail.com>
2023-08-25 02:41:31 +0530

    Home Component ( COMPLETED )

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import About from './components/about/About';
 import Home from './components/home/Home';
+import Projects from './projects/Projects';
 
 function App() {
   return (
  
     <div>
-           <Navbar/>
-           {/* <Home/> */}
-           {/* <About/> */}
+           <Navbar/> 
+           <Home/>
+    
 
     </div>
 
